b7d6271fd2fe786880ece777ced6701d47304810
marycross3 <marycradick@gmail.com>
2023-03-28 17:35:41 -0500

    done with 1st studio

--- src/org/launchcode/java/demos/areaofcircle/Area.java
+++ src/org/launchcode/java/demos/areaofcircle/Area.java
@@ -7,14 +7,14 @@ public class Area {
         do {
             System.out.println("Enter a radius: ");
 
-            while (!input.hasNextDouble()) {
+            while (!input.hasNextDouble() ) {
                 System.out.println("That is not a number!");
+                System.out.println("Enter a radius: ");
                 input.next();
             }
             radius = input.nextDouble();
         } while (radius <= 0 );
             System.out.println("The area of a circle of radius "+ radius +
                     " is: " + Circle.getArea(radius));
-
     }
 }
